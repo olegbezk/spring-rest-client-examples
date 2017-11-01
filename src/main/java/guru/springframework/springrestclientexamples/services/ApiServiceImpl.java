45daa52fd0b6a23a4fa0d2c76e3c3aad187fdf68
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-11-01 15:18:26 +0200

    add rest template examples

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -12,13 +14,21 @@ public class ApiServiceImpl implements ApiService {
 
     private final RestTemplate restTemplate;
 
-    public ApiServiceImpl(final RestTemplate restTemplate) {
+    private final String apiUrl;
+
+    public ApiServiceImpl(final RestTemplate restTemplate, final @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getAllUsers(final Integer limit) {
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(apiUrl)
+                .queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
