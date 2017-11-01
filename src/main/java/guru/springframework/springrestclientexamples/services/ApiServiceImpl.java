7b1f98efd27f61da19f82364ca6abcff14f801ee
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-11-01 16:40:41 +0200

    add reactive approach to app with spring web client

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,10 +2,15 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -16,6 +21,7 @@ public class ApiServiceImpl implements ApiService {
 
     private final String apiUrl;
 
+    @Autowired
     public ApiServiceImpl(final RestTemplate restTemplate, final @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
         this.apiUrl = apiUrl;
@@ -30,5 +36,16 @@ public class ApiServiceImpl implements ApiService {
 
         UserData userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
         return userData.getData();
+    }
+
+    @Override
+    public Flux<User> getUsers(final Mono<Integer> limit) {
+        return WebClient.create(apiUrl)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(clientResponse -> clientResponse.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
     }
 }
